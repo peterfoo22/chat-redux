2d03b874c36a54b62cdff13ff3f542fc9b75710a
Peter Suh <psuh22@gmail.com>
2020-02-20 04:00:09 +0900

    updated code

--- src/reducers/messageReducer.jsx
+++ src/reducers/messageReducer.jsx
@@ -1,17 +1,6 @@
 
 const intialstate = {
-  messages: [{
-    "id": 1124,
-    "author":"anonymous92",
-    "content":"Hello world!",
-    "created_at":"2017-09-26T23:03:16.365Z"
-  },
-  {
-    "id": 233,
-    "author":"anonymous77",
-    "content":"My name is anonymous77",
-    "created_at":"2017-09-26T23:03:21.194Z"
-  }],
+  messages: [],
   channels: [ 'general', 'react', 'paris' ],
   currentUser: `anonymous${Math.floor(10 + (Math.random() * 90))}`,
   selectedChannel: 'general'
@@ -20,22 +9,15 @@ const intialstate = {
 export default function (state = intialstate, action) {
 
   switch (action.type) {
-    case 'FETCH':  {
-    let newArray = action.payload.map((element) => {
-      element
-    })
-    console.log(action.payload)
-    console.log(newArray)
-      return state
+    case 'FETCH':
 
-    }
+    return action.payload
+
     case 'SEND': {
       return state;
     }
     case 'SELECTCHANNEL': {
-      return Object.assign({}, state, {
-        selectedChannel: action.payload
-      })
+      return []
     }
     default:
       return state;
